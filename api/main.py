f12fe27cdb7bf488cb10e03c4fbf8d4e997ba304
ReggelCamp <reggel2001@gmail.com>
2025-11-19 20:30:18 +0800

    modifed dir 4

--- api/main.py
+++ api/main.py
@@ -146,8 +146,7 @@ MODEL_PATH = os.path.join(BASE_DIR, "PKL-files")
 model            = joblib.load(os.path.join(MODEL_PATH, "updated_disease_case_predictor.pkl"))
 month_encoder    = joblib.load(os.path.join(MODEL_PATH, "updated_month_encoder.pkl"))
 disease_encoder  = joblib.load(os.path.join(MODEL_PATH, "updated_disease_encoder.pkl"))
-season_encoder   = joblib.load(os.path.join(MODEL_PATH, "updated_encoder.pkl"))
-
+season_encoder   = joblib.load(os.path.join(MODEL_PATH, "updated_season_encoder.pkl")) 
 # ---------- SEASON HELPER ----------
 def get_season(month: str) -> str:
     month = month.lower()
